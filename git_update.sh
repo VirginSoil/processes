cee476ebc5acee5b4ea2019ab59baf0eea3f87f1
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-30 09:58:56 -0700

    Update git_update.sh
    Fix spacing

--- git_update.sh
+++ git_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd .. && cd ./planting_season_auth_app     && git checkout master  &&  git pull origin master
-cd .. && cd ./planting_season_API && git checkout master && git pull origin master
-cd .. && cd ./planting_season_landing_page && git checkout master && git pull origin master
+cd .. && cd ./planting_season_API          && git checkout master  && git pull origin master
+cd .. && cd ./planting_season_landing_page && git checkout master  && git pull origin master
